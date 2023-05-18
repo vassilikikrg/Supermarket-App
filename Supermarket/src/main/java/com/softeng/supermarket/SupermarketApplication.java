d361e1048112514402a752b7f565de346f087405
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2023-05-18 18:45:35 +0300

    Organized web pages' layout using 3 seperate files

--- Supermarket/src/main/java/com/softeng/supermarket/SupermarketApplication.java
+++ Supermarket/src/main/java/com/softeng/supermarket/SupermarketApplication.java
@@ -2,8 +2,10 @@ package com.softeng.supermarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.softeng")
 public class SupermarketApplication {
 
     public static void main(String[] args) {
