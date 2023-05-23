213fff1521694800253cb46b384f6811666862db
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2023-05-23 12:02:27 +0300

    Login page starters
    created a login form and html page that displays the user input in order to test the controller and the transfer of the data between the html pages and the application

--- Supermarket/src/main/java/com/softeng/supermarket/controllers/AuthenticationController.java
+++ Supermarket/src/main/java/com/softeng/supermarket/controllers/AuthenticationController.java
@@ -5,15 +5,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthenticationController {
 
-    @GetMapping("/")
+    @GetMapping("/login")
     public String displayLoginForm(Model model){
         model.addAttribute("userModel", new User());
         return "loginForm";
+    }
+    @PostMapping("/processLogin")
+    public String processLogin(User userModel,Model model){
+        model.addAttribute("userModel",userModel);
+        return "loginTest";
     }
 }
