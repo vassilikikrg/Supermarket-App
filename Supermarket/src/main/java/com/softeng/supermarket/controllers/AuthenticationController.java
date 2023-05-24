ff7d24e5a9a27b22bd1b30c1dddba0f57b6489c8
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2023-05-24 11:00:40 +0300

    login page + login controller
    The login page displays error messages in case of an invalid format of the user input. The login controller contains a function that directs to the login form and another one that checks if the user input has the correct format. In the case that it does, it directs the user to a loginTest page. Otherwise, it redirects them to the login form, which now contains error messages.

--- Supermarket/src/main/java/com/softeng/supermarket/controllers/AuthenticationController.java
+++ Supermarket/src/main/java/com/softeng/supermarket/controllers/AuthenticationController.java
@@ -1,8 +1,10 @@
 package com.softeng.supermarket.controllers;
 
 import com.softeng.supermarket.models.User;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +20,13 @@ public class AuthenticationController {
         return "loginForm";
     }
     @PostMapping("/processLogin")
-    public String processLogin(User userModel,Model model){
+    public String processLogin(@Valid @ModelAttribute("userModel") User userModel, BindingResult bindingResult, Model model){
+        //with binding result,we take user input and send it to the controller to check if there was an error in the validation
+        if(bindingResult.hasErrors()){
+            //return the model to the form again
+            model.addAttribute("userModel",userModel);
+            return "loginForm.html";
+        }
         model.addAttribute("userModel",userModel);
         return "loginTest";
     }
--- Supermarket/src/main/java/com/softeng/supermarket/models/User.java
+++ Supermarket/src/main/java/com/softeng/supermarket/models/User.java
@@ -1,12 +1,24 @@
 package com.softeng.supermarket.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
 
+    @NotBlank(message = "Username is required")
+    @Size(min=5,max=15,message = "Username must be between 5 and 15 characters")
     private String Username;
+    @NotBlank(message = "Password is required")
+    @Size(min=8,max=15,message = "Password must be between 8 and 15 characters")
     private String Password;
+
     private String Email;
     private String Phone;
+
     private String Firstname;
+
     private String Lastname;
     private Boolean isAdmin;
 
