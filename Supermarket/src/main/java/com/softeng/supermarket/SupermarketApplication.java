1fcfde157ffeda2cb0fcae0ab14b124d6234f2a3
Antonis Fritzelas <antonyfrtz@gmail.com>
2023-06-12 22:30:05 +0300

    broken but better

--- Supermarket/src/main/java/com/softeng/supermarket/SupermarketApplication.java
+++ Supermarket/src/main/java/com/softeng/supermarket/SupermarketApplication.java
@@ -1,15 +1,32 @@
 package com.softeng.supermarket;
 
+import com.softeng.supermarket.models.User;
+import com.softeng.supermarket.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.softeng.supermarket.controllers")
+@ComponentScan("com.softeng.supermarket.models")
 @ComponentScan("com.softeng")
-public class SupermarketApplication {
+public class SupermarketApplication implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SupermarketApplication.class, args);
     }
 
+    @Override
+    public void run(String... args){
+        User user1 = new User("Antonis","test123","test@tester.com","6971989277","Antonios","Fritzelas",false);
+        userRepository.save(user1);
+        userRepository.findByUsername("Antonis").forEach(
+                val -> System.out.println(val)
+        );
+    }
 }
---
+++ Supermarket/src/main/java/com/softeng/supermarket/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.softeng.supermarket.repositories;
+
+import com.softeng.supermarket.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, String> {
+    List<User> findByUsername(String name);
+    List<User> findByPhoneNumber(String phoneNumber);
+}
