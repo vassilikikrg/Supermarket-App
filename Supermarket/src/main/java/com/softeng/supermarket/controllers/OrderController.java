2633e6ef114fbf76c393d960a8e9825c5aaa3fcc
Jimsidi <jimsid@hotmail.gr>
2023-07-04 23:52:35 +0300

    payment

--- Supermarket/src/main/java/com/softeng/supermarket/controllers/OrderController.java
+++ Supermarket/src/main/java/com/softeng/supermarket/controllers/OrderController.java
@@ -1,7 +1,14 @@
 package com.softeng.supermarket.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OrderController {
+    @GetMapping("/payment")
+    public String pay(){
+
+        return "Payment.html";
+    }
+
 }
