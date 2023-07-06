6ab05b445372eeaf77b6a603ae463dfb248a27e2
Vassiliki Karagianniki <78561945+vassilikikrg@users.noreply.github.com>
2023-07-06 20:22:37 +0300

    Restock (doesnt work yet)
    Need to find a way to get storeid and quantity on form submit

--- Supermarket/src/main/java/com/softeng/supermarket/controllers/RestockController.java
+++ Supermarket/src/main/java/com/softeng/supermarket/controllers/RestockController.java
@@ -2,6 +2,7 @@ package com.softeng.supermarket.controllers;
 
 import com.softeng.supermarket.models.Product;
 import com.softeng.supermarket.repositories.ProductRepository;
+import com.softeng.supermarket.repositories.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,13 +15,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class RestockController {
     @Autowired
     public ProductRepository productRepository;
+    @Autowired
+    public StockRepository stockRepository;
     @GetMapping("/admin/restock")
     public String showAdminPanel(Model model) {
         model.addAttribute("product", new Product());
         return "restock";
     }
     @PostMapping("/admin/performRestock")
-    public String restockProduct(@ModelAttribute Product product) {
+    public String restockProduct(@ModelAttribute Product product,@ModelAttribute("storeId") String storeId,@ModelAttribute("quantity") int quantity) {
+        //stockRepository.save(storeId,product.getId(),quantity);
+        System.out.println(storeId);
+        System.out.println(quantity);
         productRepository.save(product);
         return "redirect:/home";
     }
